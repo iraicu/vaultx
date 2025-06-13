5f132cfa94e2cdef2b99d818f70849e037845b4d
Varvara <vbondarenko@hawk.iit.edu>
2025-06-13 20:01:07 +0000

    Deleted commented out code and added a note about it

--- src/vaultx.c
+++ src/vaultx.c
@@ -37,39 +37,17 @@ void compute_hashed_key_from_plot_id()
 
 int generate_plot_id()
 {
+    // NOTE: we could also generate a priv key and derive a pub key from it. But why? 
     uint8_t public_key[32];
-    // uint8_t pubkey_compressed[33];
-    // size_t public_key_len = sizeof(pubkey_compressed);
 
     // generate 32 random bytes for the private key
     randombytes_buf(public_key, 32);
-
-    // create a secp256k1 context for signing
-    // secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    // secp256k1_pubkey pubkey;
-
-    // if (!secp256k1_ec_pubkey_create(ctx, &pubkey, private_key))
-    // {
-    //     fprintf(stderr, "Error creating public key\n");
-    //     secp256k1_context_destroy(ctx);
-    //     return -1;
-    // }
-
-    // serialize the public key
-    // secp256k1_ec_pubkey_serialize(ctx, pubkey_compressed, &public_key_len, &pubkey, SECP256K1_EC_COMPRESSED);
-
-    // concat public and private keys
-    // uint8_t combined[65];
-    // memcpy(combined, pubkey_compressed, 33);
-    // memcpy(combined + 33, private_key, 32);
 
     // compute SHA-256 of the combined buffer -> plot_id
     crypto_hash_sha256(plot_id, public_key, sizeof(public_key));
 
     // hash plot_id together with k-value to produce the key (unique plots)
     compute_hashed_key_from_plot_id();
-
-    // secp256k1_context_destroy(ctx);
 
     return 0;
 }
