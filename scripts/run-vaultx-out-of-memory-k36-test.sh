f3a44fa71880e2cf073a5cea5a3f203b46e3593a
Varvara B <vbondarenko@hawk.iit.edu>
2025-07-01 20:05:20 +0000

    local

--- scripts/run-vaultx-out-of-memory-k36-test.sh
+++ scripts/run-vaultx-out-of-memory-k36-test.sh
@@ -9,20 +9,21 @@ case $HOSTNAME in
     "epycbox")
         # max_k=34
         max_ram=131072
-        thread_num=64
+        thread_num=128
         disks=("/ssd-raid0" "/data-l" "/data-fast2")
         make_name="vaultx_x86_c"
         ;;
     "orangepi5plus")
         # max_k=31
-        max_ram=32768
+        max_ram=8192
         thread_num=8
-        disks=("/data-fast" "/data-a")
+        disks=("/data-a")
+	#disks=("/data-fast" "/data-a")
         make_name="vaultx_arm_c"
         ;;
     "raspberrypi5")
         # max_k=28
-        max_ram=4096
+        max_ram=2048
         thread_num=4
         disks=("/data-fast" "/data-a")
         make_name="vaultx_arm_c"
@@ -46,17 +47,16 @@ run_tests() {
     fi
 
     local memory=$mem_min
-    #while [ $memory -le $mem_max ]; do
-    for memory in 1280 20480; do 
-        #for i in $(seq 1 3)
-        #do
+    while [ $memory -le $mem_max ]; do
+        for i in $(seq 1 3)
+        do
             echo "Running vaultx with K=$k, memory=$memory MB, run $i ..."
             ./scripts/drop-all-caches.sh
 
             ./vaultx -a for -t $thread_num -K $k -m $memory -b 1024 -f "$mount_path/" -g "$mount_path/" -j "$mount_path/" -x true -v true >> "$data_file" & 
             vaultx_pid=$!
 
-            pidstat_log="logs/disk${disk_name}_k${k}_m${memory}_pidstat.log"
+	    pidstat_log="logs/${disk_type}_k${k}_m${memory}_pidstat.log"
             echo "----Run $i for K=$k, memory=$memory MB----" >> "$pidstat_log"
             pidstat -h -r -u -d -p $vaultx_pid 1 >> "$pidstat_log" &
 
@@ -65,9 +65,9 @@ run_tests() {
                 echo "vaultx failed with K=$k, memory=$memory MB, run $i"
             fi
 
-            rm -r "$mount_path/*"
-        #done
-        #memory=$((memory * 2))
+            rm -r plots/*.plot 
+        done
+        memory=$((memory * 2))
     done
 }
 
@@ -117,23 +117,21 @@ for disk in "${disks[@]}"; do
     #     k=32
     # fi
 
-    #make clean
-    #make $make_name NONCE_SIZE=4 RECORD_SIZE=16
+    make clean
+    make $make_name NONCE_SIZE=4 RECORD_SIZE=16
 
     # Run tests for NONCE_SIZE=4
-    #for K in $(seq 28 32); do
-    #    echo "Running tests for K=$K with $thread_num threads on $disk_name disk"
-    #    run_tests 4 $K $mount_path
-    #done
+    for K in $(seq 28 32); do
+        echo "Running tests for K=$K with $thread_num threads on $disk_name disk"
+        run_tests 4 $K $mount_path
+    done
 
     make clean
     make $make_name NONCE_SIZE=5 RECORD_SIZE=16
 
-    run_tests 5 36 $mount_path
-
     # Run tests for NONCE_SIZE=5
-    #for K in $(seq 33 $max_k); do
-    #    echo "Running tests for K=$K with $thread_num threads on $disk_name disk"
-    #    run_tests 5 $K $mount_path
-    #done
+    for K in $(seq 33 $max_k); do
+        echo "Running tests for K=$K with $thread_num threads on $disk_name disk"
+        run_tests 5 $K $mount_path
+    done
 done
