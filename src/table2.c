b4d56e6645b89013fa21376f107f169b1d726906
Varvara <vbondarenko@hawk.iit.edu>
2025-07-10 20:42:39 +0000

    Changing read batch size affects storage efficiency

--- src/table2.c
+++ src/table2.c
@@ -420,10 +420,8 @@ size_t process_memo_records_table2(const char *filename, const size_t BATCH_SIZE
 }
 
 void generate_table2(MemoRecord *sorted_records, size_t num_records_in_bucket)
-// void generate_table2(Bucket *bucket, size_t num_records_in_bucket)
 {
     uint64_t expected_distance = 1ULL << (64 - K);
-    // expected_distance = expected_distance * 10;
 
     // num_records_in_bucket = num_records_in_shuffled_bucket = num_records_in_bucket * rounds
     for (size_t i = 0; i < num_records_in_bucket; ++i)
