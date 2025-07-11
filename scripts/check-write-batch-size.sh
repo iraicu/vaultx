d6390cf5bf12e2948e537305b051bda23402f860
udig-v <vbondarenko@hawk.iit.edu>
2025-07-12 02:14:36 +0800

    general check write batch script

--- scripts/check-write-batch-size.sh
+++ scripts/check-write-batch-size.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-disks=('/ssd-raid0' '/data-l')
+HOSTNAME=$(hostname)
+
+case $HOSTNAME in
+	"epycbox")
+		disks=('/ssd-raid0' '/data-l')
+		;;	
+	"orangepi5plus")
+		disks=('/data-fast' '/data-a')
+		;;
+	"raspberrypi5")
+		disks=('/data-fast' '/data-a')
+		;;
+	*)
+		echo "Machine is undefined"
+		exit 1
+		;;
+esac
 
 make vaultx_x86_c NONCE_SIZE=4 RECORD_SIZE=16
 
@@ -22,22 +38,25 @@ for disk in "${disks[@]}"; do
             ;;
     esac
 
-    data_file="data/$hostname-$disk_type-write-batch-size-check1.txt"
+    data_file="data/$hostname-$disk_type-write-batch-size-check.txt"
 
     echo "Starting write batch size check for disk: $disk"
     echo "Data will be saved to: $data_file"
 
     echo "APPROACH,K,sizeof(MemoRecord),num_threads,MEMORY_SIZE(MB),file_size(GB),BATCH_SIZE,total_throughput(MH/s),total_throughput(MB/s),elapsed_time_hash_total,elapsed_time_hash2_total,elapsed_time_io_total,elapsed_time_shuffle_total,other_time,elapsed_time,storage_efficiency" > "$data_file"
 
+    ./scripts/drop-all-caches.sh
+    ./vaultx -v true -a for -f "$disk/varvara/plot/" -g "$disk/varvara/plot/" -j "$disk/varvara/plot/" -t 64 -K 29 -m 512 -W 1 -R 1 -x true >> "$data_file"
+
     # Run the vaultx command with the specified parameters
-    #for ((W=1024; W <= 16777216; W*=2)) ; do
-    #    echo "Running with WRITE_BATCH_SIZE=$W"
+    for ((W=1024; W <= 16777216; W*=2)) ; do
+        echo "Running with WRITE_BATCH_SIZE=$W"
         for i in {1..3}; do
             echo "Iteration $i for WRITE_BATCH_SIZE=$W"
             ./scripts/drop-all-caches.sh
-            ./vaultx -v true -a for -f "$disk/varvara/plot/" -g "$disk/varvara/plot/" -j "$disk/varvara/plot/" -t 64 -K 29 -m 512 -W 1 -R 1 -x true >> "$data_file"
+            ./vaultx -v true -a for -f "$disk/varvara/plot/" -g "$disk/varvara/plot/" -j "$disk/varvara/plot/" -t 64 -K 29 -m 512 -W "$W" -R 1 -x true >> "$data_file"
         done
-    #done
+    done
     
     echo "Write batch size check completed for disk: $disk"
 done
